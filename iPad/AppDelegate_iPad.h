45a0796810e05f3418bbf0a47f7b3311a411de4d
balabhadra <bal.friendly@gmail.com>
2011-08-23 17:58:56 +0545

    Initial import with project skeleton.

---
+++ iPad/AppDelegate_iPad.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate_iPad.h
+//  Playsongs
+//
+//  Created by Bala Bhadra Maharjan on 8/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate_Shared.h"
+
+@interface AppDelegate_iPad : AppDelegate_Shared {
+}
+
+
+@end
+
---
+++ iPhone/AppDelegate_iPhone.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate_iPhone.h
+//  Playsongs
+//
+//  Created by Bala Bhadra Maharjan on 8/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate_Shared.h"
+
+@interface AppDelegate_iPhone : AppDelegate_Shared {
+}
+
+
+@end
+
